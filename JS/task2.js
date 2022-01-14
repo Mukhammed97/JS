a2b6dc849051ed77a7e85c0dbe1156c4454769c5
Mukhammadamin Dzhanybai uulu <mukhammadamin.dzhanybai.on@gmail.com>
2022-01-15 02:33:04 +0600

    second commit

---
+++ JS/task2.js
@@ -0,0 +1,5 @@
+let firstName = "js";
+let firstChar = firstName[0].toUpperCase();
+let theRest = firstName.slice(1);
+let formattedFirstName = firstChar + theRest;
+console.log(formattedFirstName);
